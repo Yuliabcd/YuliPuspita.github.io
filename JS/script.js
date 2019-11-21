43d1d169d701162480a435c3c4fc05defe3d9596
Yuliabcd <55028240+Yuliabcd@users.noreply.github.com>
2019-11-21 10:18:42 +0700

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+// modal masuk
+$(document).ready(function(){
+	$('#masuk').click(function(){
+		$('.trans-bg-login').show();
+	})
+	$('.trans-bg-login').click(function(){
+		$('.trans-bg-login').hide();
+	})
+})
+// modal daftar
+$(document).ready(function(){
+	$('#daftar').click(function(){
+		$('.trans-bg-daftar').show();
+	})
+	$('.trans-bg-daftar').click(function(){
+		$('.trans-bg-daftar').hide();
+	})
+})
